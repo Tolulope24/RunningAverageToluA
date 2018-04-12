b81ce3d023e81788f96f20e2b3b78f706f4c937b
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-04-12 13:31:10 -0400

    Initial commit

---
+++ RunningAverageForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RunningAverageToluA
+{
+    partial class frmRunningAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblStatement = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblStatement
+            // 
+            this.lblStatement.AutoSize = true;
+            this.lblStatement.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatement.Location = new System.Drawing.Point(12, 34);
+            this.lblStatement.Name = "lblStatement";
+            this.lblStatement.Size = new System.Drawing.Size(240, 16);
+            this.lblStatement.TabIndex = 1;
+            this.lblStatement.Text = "Enter a Number (from 0 to 100)";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(12, 142);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Location = new System.Drawing.Point(12, 67);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(131, 16);
+            this.lblExit.TabIndex = 3;
+            this.lblExit.Text = "Press \"-1\" to Exit";
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(258, 32);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(47, 20);
+            this.txtAverage.TabIndex = 4;
+            // 
+            // frmRunningAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 257);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblStatement);
+            this.Controls.Add(this.button1);
+            this.Name = "frmRunningAverage";
+            this.Text = "Running Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblStatement;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.TextBox txtAverage;
+    }
+}
+
---
+++ RunningAverageForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tolulope Adebayo
+ * Created on: 11-04-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Running Average
+ * This program keeps accepting input (a mark between 0 and 100) and calculates the average
+*/
+namespace RunningAverageToluA
+{
+    public partial class frmRunningAverage : Form
+    {
+        public frmRunningAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Identify the 
+            int Average;
+            Average = int.Parse(txtAverage.Text);
+        }
+    }
+}
